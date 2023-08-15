c4b04a43760fe39f3f5168c4d6857412376afd5d
Felipe Estrada <felipe.estrada.prada@gmail.com>
2023-08-14 20:00:50 -0500

    feat: se agregó función mostrarMensaje EJ5

---
+++ js/ejercicio5.js
@@ -0,0 +1,10 @@
+/*
+Crea una función llamada mostrarMensaje que tome un parámetro nombre. Dentro de la función, utiliza alert() para mostrar: "¡Hola, [nombre]! Espero que estés disfrutando de JavaScript." Pide al usuario su nombre utilizando prompt() y llama a la función.
+(onclick)
+*/
+
+function mostrarMensaje() {
+    let nombre = prompt("¿Cómo te llamas?")
+
+    alert("¡Hola, " + nombre + "! Espero que estés disfrutando de JavaScript.")
+}
